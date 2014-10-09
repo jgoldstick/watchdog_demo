9bdeb044e568703217611578b3bc6bbc6c4756c4
Joel Goldstick <joel.goldstick@gmail.com>
2014-10-09 12:37:10 -0400

    modified:   watchdog_demo.py

--- watchdog_demo.py
+++ watchdog_demo.py
@@ -1,4 +1,12 @@
 #! /usr/bin/env python
+"""
+Code adapted from watchdog demo https://github.com/gorakhargosh/watchdog
+
+Usage:
+    python watchdog_demo.py [-r] <dir>
+    where -r will watch recursively, <dir> is the directory to watch
+    
+"""
 
 import sys
 import time
@@ -7,13 +15,26 @@ from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 
 if __name__ == "__main__":
+    args = len(sys.argv)
+    if args == 3:
+        if sys.argv[1] == '-r':
+            recurse = True
+        path = sys.argv[2]
+    if args == 2:
+        recurse = False
+        path = sys.argv[1]
+    if args == 1:
+        print """Usage:\n\t
+            python watchdog_demo.py [-r] <dir>  
+            where -r will watch recursively, <dir> is the directory to watch"""
+        exit()
+        
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
     event_handler = LoggingEventHandler()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, path, recursive=recurse)
     observer.start()
     try:
         while True:
